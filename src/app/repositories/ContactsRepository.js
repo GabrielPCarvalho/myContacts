9593721c1163920149d3c16551f3dad930e2d79a
GabrielPCarvalho <gabriel57_pereira.com>
2023-07-13 21:49:56 -0300

    feat: get users, get user and delete user

---
+++ src/app/repositories/ContactsRepository.js
@@ -0,0 +1,42 @@
+const { v4 } = require('uuid');
+
+let contacts = [
+  {
+    id: v4(), // 'uuid -> Universal Unique ID',
+    name: 'Gabriel',
+    email: 'gabriel@gmail.com',
+    phone: '123456789',
+    category_id: v4(),
+  },
+  {
+    id: v4(), // 'uuid -> Universal Unique ID',
+    name: 'Amanda',
+    email: 'amanda@gmail.com',
+    phone: '228882288',
+    category_id: v4(),
+  },
+];
+
+class ContactsRepository {
+  findAll() {
+    return new Promise((resolve) => {
+      resolve(contacts);
+    });
+  }
+
+  findById(id) {
+    return new Promise((resolve) => {
+      resolve(contacts.find((contact) => contact.id === id));
+    });
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+
+      resolve();
+    });
+  }
+}
+
+module.exports = new ContactsRepository();
