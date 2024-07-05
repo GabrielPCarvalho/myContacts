cc5e8d16c8fa52819dd7585f5ff041a92e8a07de
GabrielPCarvalho <gabriel57_pereira@hotmail.com>
2024-07-05 08:26:42 -0300

    refactor: validate form

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Form, ButtonContainer } from './styles';
 import PropTypes from 'prop-types';
-
 import FormGroup from "../FormGroup"
 import Input from "../Input"
 import Select from "../Select"
 import Button from "../Button"
+import CategoriesService from '../../services/CategoriesService';
+import isEmailValid from '../../utils/isEmailValid';
+import formatPhone from '../../utils/formatPhone';
+import useErrors from '../../hooks/useErrors';
 
 
 
@@ -13,51 +16,102 @@ const ContactForm = ({ buttonLabel }) => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
-  const [category, setCategory] = useState('');
+  const [categoryId, setCategoryId] = useState('');
+  const [categories, setCategories] = useState([]);
+  const {
+    setError,
+    getErrorMessageByFieldName,
+    removeError,
+    errors
+  } = useErrors();
 
-  function handleSubmit(event) {
-    event.preventDefault();
+  const isFormValid = (name && errors.length === 0);
+
+  function handleNameChange(e){
+    setName(e.target.value)
+
+    if (!e.target.value) {
+      setError({ field: 'name', message: 'Nome é obrigatório' });
+    } else {
+      removeError('name');
+    }
   }
-  //TODO -  Validando os dados do input de nome
+
+  function handleEmailChange(e){
+    setEmail(e.target.value)
+
+    if (e.target.value && !isEmailValid(e.target.value)) {
+      setError({ field: 'email', message: 'Email invalido' });
+    } else {
+      removeError('email');
+    }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  function handlePhoneChange(e) {
+    setPhone(formatPhone(e.target.value));
+  }
+
+  useEffect(() => {
+    async function loadCategories() {
+      const categories = await CategoriesService.listCategories();
+      setCategories(categories);
+    }
+
+    loadCategories();
+  },[]);
+
   return (
-    <Form method='post' onSubmit={handleSubmit}>
-      <FormGroup>
+    <Form method='post' onSubmit={handleSubmit} noValidate>
+      <FormGroup error={getErrorMessageByFieldName('name')}>
         <Input
           value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder='Nome'
+          onChange={handleNameChange}
+          placeholder='Nome *'
+          error={getErrorMessageByFieldName('name')}
         />
       </FormGroup>
 
-      <FormGroup>
+      <FormGroup error={getErrorMessageByFieldName('email')}>
         <Input
+          type='email'
           value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={handleEmailChange}
           placeholder='E-mail'
+          error={getErrorMessageByFieldName('email')}
         />
       </FormGroup>
 
       <FormGroup>
         <Input
           value={phone}
-          onChange={(e) => setPhone(e.target.value)}
+          onChange={handlePhoneChange}
           placeholder='Telefone'
+          maxLength={15}
         />
       </FormGroup>
 
       <FormGroup>
         <Select
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
+          value={categoryId}
+          onChange={(e) => setCategoryId(e.target.value)}
         >
-          <option value=''>Categoria</option>
-          <option value='Instagram'>Instagram</option>
-          <option value='Facebook'>Facebook</option>
+          <option value=''>Sem categoria</option>
+
+          {categories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+
         </Select>
       </FormGroup>
 
       <ButtonContainer>
-      <Button type='submit'>
+      <Button type='submit' disabled={!isFormValid}>
         {buttonLabel}
       </Button>
       </ButtonContainer>
