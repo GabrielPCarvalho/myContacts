5e283064c955eb9d35a178c51f6a9dec41815c62
GabrielPCarvalho <gabriel57_pereira@hotmail.com>
2024-08-30 14:42:01 -0300

    refactor: adjust toast

--- src/components/Toast/ToastContainer/index.jsx
+++ src/components/Toast/ToastContainer/index.jsx
@@ -1,39 +1,43 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useCallback } from "react"
 import { Container } from "./styles"
 import { ToastMessage } from "../ToastMessage"
+import { toastEventManager } from "../../../utils/toast"
 
 export default function ToastContainer() {
   const [messages, setMessages] = useState([])
 
   useEffect(() => {
-    function handleAddToast(event) {
-      const { type, text } = event.detail;
+    function handleAddToast({ type, text, duration }) {
 
       setMessages((prevState) => [
         ...prevState,
         {
           id: Math.random(),
           type,
-          text
+          text,
+          duration
         }
 
       ])
     }
 
-    document.addEventListener('addtoast', handleAddToast)
-
+    toastEventManager.on('addtoast', handleAddToast)
     return () => {
-      document.removeEventListener('addtoast', handleAddToast)
+      toastEventManager.removeListener('addtoast', handleAddToast)
     }
   },[]);
+
+  const handleRemoveMessage = useCallback((id) => {
+   setMessages((prevState) => prevState.filter((message) => message.id !== id))
+  }, []);
 
   return (
     <Container>
       {messages.map((message) => (
         <ToastMessage
           key={message.id}
-          type={message.type}
-          text={message.text}
+          message={message}
+          onRemoveMessage={handleRemoveMessage}
         />
       ))}
     </Container>
